c681834cdfdf2b67403cfe30341afa0e7d1f43f5
Zac Cericola <zcericola@gmail.com>
2018-02-21 12:30:09 -0600

    added stuff

--- server/strategy.js
+++ server/strategy.js
@@ -5,6 +5,7 @@ module.exports = new Auth0Strategy ({
     domain: domain,
     clientID: clientID,
     clientSecret: clientSecret,
+    scope: "profile openid",
     callbackURL: '/login'
 
 }, function(accessToken, refreshToken, extraParams, profile, done){
